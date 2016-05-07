c68d1ff5f03020b38fc5f376f5ee262e36def13f
Lútsen Stellingwerff <lutsen@hoverkraft.nl>
2016-05-07 17:47:12 +0100

    First!

---
+++ String.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Lagan\Property;
+
+/**
+ * Controller for the Lagan string property.
+ * Doesn't do anything.
+ *
+ * A property type controller can contain a set, read, delete and options method. All methods are optional.
+ * To be used with Lagan: https://github.com/lutsen/lagan
+ */
+
+class String {}
+
+?>
